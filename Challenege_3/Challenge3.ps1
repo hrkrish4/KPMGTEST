d1b814e28d6342cb322606f0387cafc87c865493
maharikr <malempati.hari-krishna@capgemini.com>
2023-01-22 19:00:29 +0530

    change3

---
+++ Challenege_3/Challenge3.ps1
@@ -0,0 +1,23 @@
+﻿function Get-InnermostHashtableValue {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline=$true)]
+        [System.Collections.Hashtable]$hashtable
+    )
+
+    if ($hashtable.Count -eq 0) {
+        return $null
+    }
+
+    foreach ($key in $hashtable.Keys) {
+        if ($hashtable[$key] -is [System.Collections.Hashtable]) {
+            return Get-InnermostHashtableValue $hashtable[$key]
+        } 
+    }
+    return $hashtable
+}
+
+
+$inputHash = @{'x'=@{'y'=@{'z'='a'}}}
+$value = Get-InnermostHashtableValue $inputHash
+$value
