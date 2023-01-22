17793c20ee67dce6401e27b0350851cf0ab20bdc
maharikr <malempati.hari-krishna@capgemini.com>
2023-01-22 09:04:23 +0530

    metadata

--- Challenge_2/InstancemetadaVM.ps1
+++ Challenge_2/InstancemetadaVM.ps1
@@ -1,10 +1,10 @@
 ﻿$ImdsServer = "http://169.254.169.254"
 $InstanceEndpoint = $ImdsServer + "/metadata/instance"
-
+$uri = $InstanceEndpoint + "?api-version=2021-02-01"
 
 $Proxy=New-object System.Net.WebProxy
 $WebSession=new-object Microsoft.PowerShell.Commands.WebRequestSession
 $WebSession.Proxy=$Proxy
-$output=Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01" -WebSession $WebSession
+$output=Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri $uri -WebSession $WebSession
 
 $output | ConvertTo-JSON 
